c76ffd06eccc48524803d34bbc79d37a45f042c4
pixsus228 <alexkarnauch759@gmail.com>
2026-06-26 18:58:46 +0300

    Ваше повідомлення про зміни

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-from .views import HomeView, ProductListView, ProductDetailView
+﻿from django.urls import path
+from .views import HomeView, ProductListView, ProductDetailView, ProductListAPIView
 
-app_name = "shop"
+app_name = 'shop'
+
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
-    path("products/", ProductListView.as_view(), name="product_list"),
-    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path('', HomeView.as_view(), name='home'),
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('api/products/', ProductListAPIView.as_view(), name='product_list_api'),
 ]
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import ListView, DetailView, TemplateView
+﻿from rest_framework import generics
 from .models import Product
+from .serializers import ProductSerializer
+
+# Стандартні класи для веб-сайту
+from django.views.generic import ListView, DetailView, TemplateView
 
 class HomeView(TemplateView):
     template_name = 'shop/home.html'
@@ -13,3 +17,8 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'shop/product_detail.html'
     context_object_name = 'product'
+
+# API-представлення
+class ProductListAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
